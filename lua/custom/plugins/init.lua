5d25133d1108104037ccfef5cdefbee70d4f57ff
Faiyaz Hasan <faiyaz.hasan1@pm.me>
2025-12-17 23:10:40 -0500

    Add floating terminal

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,21 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'numToStr/FTerm.nvim',
+    config = function()
+      require('FTerm').setup {
+        border = 'rounded',
+        dimensions = {
+          height = 0.9,
+          width = 0.9,
+        },
+      }
+
+      -- Keybinding to toggle terminal with <leader>tt
+      vim.keymap.set('n', '<leader>tt', '<CMD>lua require("FTerm").toggle()<CR>', { desc = '[T]oggle [T]erminal' })
+      vim.keymap.set('t', '<leader>tt', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', { desc = '[T]oggle [T]erminal' })
+    end,
+  },
+}
